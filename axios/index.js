6358fa3c8b0de13417589df8993194c02a7f1b19
yzirianov <y.zirianov@method.me>
2021-10-03 12:07:41 -0400

    API Get call

---
+++ axios/index.js
@@ -0,0 +1,24 @@
+module.exports = require('./lib/axios');
+const axios = require('axios');
+
+async function makeGetRequest() {
+
+
+  let res = await axios.get('https://datausa.io/api/data?drilldowns=Nation&measures=Population');
+
+  let response = res.data.data;
+
+  //console.log(response);
+ for (let i=0;i<response.length;i++ ){
+     if (response[i].Year==='2019' && response[i].Population===328239523){
+        
+            console.log(`The Population of USA is ${response[i].Population} in the Year ${response[i].Year}    `)
+            break
+     }
+     
+
+    }
+} 
+
+makeGetRequest();
+
